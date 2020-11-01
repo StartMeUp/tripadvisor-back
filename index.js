edbefb6f1b3c634cf022456b8c261b8ae31b5348
StartMeUpLille <contact@start-me-up.fr>
2020-11-01 14:44:16 +0100

    backend working well

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
 require("dotenv").config();
 const express = require("express");
 const formidable = require("express-formidable");
-//const cors = require("cors");
+const cors = require("cors");
 const app = express();
-//app.use(cors());
+app.use(cors());
 app.use(formidable());
 
-// /* MAILGUN CONFIGURATION */
-// const api_key = process.env.MAILGUN_API_KEY; /* VOTRE CLÉ API */
-// const domain = process.env.MAILGUN_DOMAIN; /* VOTRE DOMAINE SANDBOX */
+/* MAILGUN CONFIGURATION */
+const api_key = process.env.MAILGUN_API_KEY; /* VOTRE CLÉ API */
+const domain = process.env.MAILGUN_DOMAIN; /* VOTRE DOMAINE SANDBOX */
 
-// const mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+const mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
 
 app.get("/", (req, res) => {
   res.send("server is up");
 });
 
-// app.post("/", (req, res) => {
-//   /* DESTRUCTURING */
-//   const { name, email, password } = req.fields;
-//   /* CREATION DE L'OBJET DATA */
-//   const data = {
-//     from: `${name} <${email}>`,
-//     to: process.env.MY_EMAIL, /* EMAIL AVEC LAQUELLE VOUS VOUS ÊTES ENREGISTRÉS SUR MAILGUN */
-//     subject: subject,
-//     text: message,
-//   };
+app.post("/", (req, res) => {
+  /* DESTRUCTURING */
+  const { name, email, password } = req.fields;
+  /* CREATION DE L'OBJET DATA */
+  const data = {
+    from: `${name} <${email}>`,
+    to: process.env.MY_EMAIL, /* EMAIL AVEC LAQUELLE VOUS VOUS ÊTES ENREGISTRÉS SUR MAILGUN */
+    subject: "Tripadvisor signup",
+    text: `Your signup was successful with the password: ${password} !`,
+  };
 
-//   /* ENVOI DE L'OBJET VIA MAILGUN */
-//   mailgun.messages().send(data, (error, body) => {
-//     if (!error) {
-//       return res.json(body);
-//     }
-//     res.status(401).json(error);
-//   });
-// });
+  /* ENVOI DE L'OBJET VIA MAILGUN */
+  mailgun.messages().send(data, (error, body) => {
+    if (!error) {
+      return res.json(body);
+    }
+    res.status(401).json(error);
+  });
+});
 
 app.listen(process.env.PORT, () => {
   console.log("server is listening");
